e6ac053577760e44c09dfe617eaf1f9511aeb7d5
kkutchan <kkutchan@foxconndrc.com>
2019-02-19 20:07:30 +0100

    responsive table structure

--- cnb-app/src/App.js
+++ cnb-app/src/App.js
@@ -6,8 +6,28 @@ class App extends Component {
 	render() {
 		return (
 			<Fragment>
-				<header className="App-header">
+				<header className="cg-flexBox header">
+					<h1 className="cg-ta-c">Current exchange rates from ČNB</h1>
 				</header>
+				<h2 className="cg-ta-c cg-w100">Data actual for date: #</h2>
+				<div className="cg-table">
+					<div className="cg-table__head">
+						<div className="cg-tableHead__cell">1</div>
+						<div className="cg-tableHead__cell">1</div>
+						<div className="cg-tableHead__cell">1</div>
+						<div className="cg-tableHead__cell">1</div>
+						<div className="cg-tableHead__cell">1</div>
+					</div>
+					<div className="cg-table__body cg-tableBody">
+						<div className="cg-tableBody__row">
+							<div className="cg-tableBody__row__cell">2</div>
+							<div className="cg-tableBody__row__cell">2</div>
+							<div className="cg-tableBody__row__cell">2</div>
+							<div className="cg-tableBody__row__cell">2</div>
+							<div className="cg-tableBody__row__cell">2</div>
+						</div>
+					</div>
+				</div>
 			</Fragment>
 		);
 	}
